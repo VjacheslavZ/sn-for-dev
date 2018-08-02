b0cdc98c20716a703878d71c9ea871e2d1ab60fb
VjacheslavZ <slavikov.net@mail.ru>
2018-08-03 00:17:03 +0300

    add react and base marckup

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+
 //Post model
 const Post = require('../../models/Post');
 //Profile model
 const Profile = require('../../models/Profile');
-
 //Validation post
 const validatePostInput = require('../../validation/post');
-
 
 // @route GET api/posts/test
 // @desc  Tests post route
@@ -126,6 +125,57 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
 				})
 				.catch(err => res.status(404).json({ postnotfound: 'No post found'}));
 		})
+});
+
+// @route POST api/posts/comment/:id
+// @desc  Add comment to post
+// @access Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const { errors, isValid } = validatePostInput(req.body);
+	//Check validation
+	if(!isValid) {
+		//If any errors, send 400 with errors object
+		return res.status(400).json(errors);
+	}
+	Post.findById(req.params.id)
+		.then(post => {
+			const newComment = {
+				text: req.body.text,
+				name: req.body.name,
+				avatar: req.body.avatar,
+				user: req.user.id
+			};
+
+			//Add to comments array
+			post.comments.unshift(newComment);
+
+			//Save
+			post.save().then(post => res.json(post))
+		})
+		.catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
+// @route Delete api/posts/comment/:id/:comment_id
+// @desc  Remove comment from post
+// @access Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Post.findById(req.params.id)
+		.then(post => {
+			//Check to se if the comment exists
+			if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+				return res.status(404).json({ comment: 'Comment does not exists' })
+			}
+
+			//Get remove index
+			const removeindex = post.comments
+				.map(item => item._id.toString())
+				.indexOf(req.params.comment_id);
+
+			//Splice comment form array
+			post.comments.splice(removeindex, 1);
+			post.save().then(post => res.json(post))
+		})
+		.catch(err => res.status(404).json({ postnotfound: 'No post found' }));
 });
 
 module.exports = router;
