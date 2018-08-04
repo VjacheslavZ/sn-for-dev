cbd19b031c88dc645fd14589dcfc4d7f6dd49855
VjacheslavZ <slavikov.net@mail.ru>
2018-08-05 00:41:19 +0300

    add store authReducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux'
+import store from './store';
 import './App.css';
 
 import Navbar from './components/layout/Navbar';
@@ -8,23 +10,26 @@ import Landing from './components/layout/Landing';
 import Regiser from './components/auth/Register';
 import Login from './components/auth/Login';
 
+
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div className="App">
-                    <Navbar />
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Navbar />
 
-                    <Route exact path='/' component={Landing}/>
+                        <Route exact path='/' component={Landing}/>
 
-                    <div className="container">
-                        <Route exact path='/register' component={Regiser}/>
-                        <Route exact path='/login' component={Login}/>
+                        <div className="container">
+                            <Route exact path='/register' component={Regiser}/>
+                            <Route exact path='/login' component={Login}/>
+                        </div>
+
+                        <Footer />
                     </div>
-
-                    <Footer />
-                </div>
-            </Router>
+                </Router>
+            </Provider>
         );
     }
 }
