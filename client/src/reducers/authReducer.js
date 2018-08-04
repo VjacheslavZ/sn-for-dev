27d9bff905c3142e6fd6e0db544bde7b0bc0ea3d
VjacheslavZ <slavikov.net@mail.ru>
2018-08-05 01:12:27 +0300

    auth reducer continue

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,12 +1,16 @@
+import {TEST_DISPATCH} from '../actions/types';
 const initialState = {
     isAuthenticated: false,
     user: {}
 };
 
-export default function (state=initialState, action) {
+export default function (state = initialState, action) {
     switch (action.type) {
         case "SET_CURRENT_USER":
-
+            return {
+                ...state,
+                user: action.payload
+            };
         default:
             return state;
     }
