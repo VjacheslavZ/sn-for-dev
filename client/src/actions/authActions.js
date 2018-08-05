501200e7a198d1ffc62ba25cd185e211a3651d76
VjacheslavZ <slavikov.net@mail.ru>
2018-08-05 14:14:56 +0300

    l41 add auth action and handler errors

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,16 @@
-import {TEST_DISPATCH} from './types'
-//Register
-export const registerUser = (userData) => {
-    return {
-        type: TEST_DISPATCH,
-        payload: userData
-    }
+import axios from 'axios';
+import {GET_ERRORS} from './types';
+//Register User
+export const registerUser = (userData, history)=> dispatch => {
+
+    axios.post('/api/users/register', userData)
+    	.then(res => history.push('/login'))
+    	.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		)
 };
+
+// this.props.history.push('/dasboard');
