1d250a80cc9b182d34a38f4e3781e072b1418861
VjacheslavZ <slavikov.net@mail.ru>
2018-08-09 01:23:57 +0300

    051 Create Profile Form ++

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+
 import TextFieldCroup from '../common/TextFieldGroup';
+import SelectListCroup from '../common/SelectListGroup';
 import TextAreaFieldCroup from '../common/TextAreaFiedGroup';
 import InputCroup from '../common/InputGroup';
-import SelectListCroup from '../common/SelectListGroup';
 
 class CreateProfile extends Component {
 	constructor(props) {
@@ -26,10 +27,88 @@ class CreateProfile extends Component {
 			youtube: '',
 			instagram: '',
 			errors: {}
-		}
+		};
+
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		console.log('submit');
+	}
+
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value })
 	}
 
 	render() {
+		const {errors, displaySocialInputs} = this.state;
+
+		let socialInputs;
+		if(displaySocialInputs) {
+			socialInputs = (
+				<div>
+					<InputCroup
+						placeholder='Twiiter Profile URL'
+						name='twitter'
+						icon='fab fa-twitter'
+						value={this.state.twitter}
+						onChange={this.state.onChange}
+						error={errors.twitter}
+					/>
+
+					<InputCroup
+						placeholder='Facebook Page URL'
+						name='facebook'
+						icon='fab fa-facebook'
+						value={this.state.facebook}
+						onChange={this.state.onChange}
+						error={errors.facebook}
+					/>
+
+					<InputCroup
+						placeholder='Linkedin Page URL'
+						name='linkedin'
+						icon='fab fa-linkedin'
+						value={this.state.linkedin}
+						onChange={this.state.onChange}
+						error={errors.linkedin}
+					/>
+
+					<InputCroup
+						placeholder='YouTube Chanel URL'
+						name='youtube'
+						icon='fab fa-youtube'
+						value={this.state.youtube}
+						onChange={this.state.onChange}
+						error={errors.youtube}
+					/>
+
+					<InputCroup
+						placeholder='Instagram Chanel URL'
+						name='instagram'
+						icon='fab fa-youtube'
+						value={this.state.instagram}
+						onChange={this.state.onChange}
+						error={errors.instagram}
+					/>
+				</div>
+			)
+		}
+		//Select options for status
+		const options = [
+			{label: 'Select Professional Status', value: 0},
+			{label: 'Developer', value: 'Developer'},
+			{label: 'Junior Developer', value: 'Junior Developer'},
+			{label: 'Senior Developer', value: 'Senior Developer'},
+			{label: 'Manager', value: 'Manager'},
+			{label: 'Student or Learning', value: 'Student or Learning'},
+			{label: 'Instructor or Teacher', value: 'Instructor or Teacher'},
+			{label: 'Intern', value: 'Intern'},
+			{label: 'Other', value: 'Other'},
+		];
+
 		return (
 			<div className='create-profile'>
 				<div className="container">
@@ -40,6 +119,95 @@ class CreateProfile extends Component {
 								Let's get some information to make your profile stand out
 							</p>
 							<small className="d-block pb-3">* = required fields</small>
+
+							<form onSubmit={this.onSubmit}>
+								<TextFieldCroup
+									placeholder='* Profile handle'
+									name='handle'
+									value={this.state.handle}
+									onChange={this.onChange}
+									error={errors.handle}
+									info="A unique handle for your profile URL. Your full name, company name, nicname"
+								/>
+
+								<SelectListCroup
+									placeholder='Status'
+									name='status'
+									value={this.state.status}
+									onChange={this.onChange}
+									options={options}
+									error={errors.status}
+									info="Give us an idea of where you are at in your career"
+								/>
+
+								<TextFieldCroup
+									placeholder='Company'
+									name='company'
+									value={this.state.company}
+									onChange={this.onChange}
+									error={errors.company}
+									info="Could be your own company or one you work for"
+								/>
+
+								<TextFieldCroup
+									placeholder='Website'
+									name='website'
+									value={this.state.website}
+									onChange={this.onChange}
+									error={errors.website}
+									info="Could be your own website or a company one"
+								/>
+
+								<TextFieldCroup
+									placeholder='Location'
+									name='location'
+									value={this.state.location}
+									onChange={this.onChange}
+									error={errors.location}
+									info="City or city & state suggested (eg. Boston, MA)"
+								/>
+
+								<TextFieldCroup
+									placeholder='* Skills'
+									name='skills'
+									value={this.state.skills}
+									onChange={this.onChange}
+									error={errors.skills}
+									info="Please use comma separated values (eg. HTML,CSS,JS,PHP)"
+								/>
+
+								<TextFieldCroup
+									placeholder='Github Username'
+									name='githubusername'
+									value={this.state.githubusername}
+									onChange={this.onChange}
+									error={errors.githubusername}
+									info="If you want your latest repos and a Github link, include your usernme"
+								/>
+
+								<TextAreaFieldCroup
+									placeholder='Short Bio'
+									name='bio'
+									value={this.state.bio}
+									onChange={this.onChange}
+									error={errors.bio}
+									info="Tell us a little about yourself"
+								/>
+
+								<div className="mb-3">
+									<button onClick={() => {
+										this.setState(prevState => ({
+											displaySocialInputs: !prevState.displaySocialInputs
+										}))
+									}} className="btn btn-light">
+										Add Social Network links
+									</button>
+									<span className='text-muted'>Optional</span>
+								</div>
+
+								{socialInputs}
+								<input type="submit" value='Submit' className='btn btn-info btn-block mt-4'/>
+							</form>
 						</div>
 					</div>
 				</div>
