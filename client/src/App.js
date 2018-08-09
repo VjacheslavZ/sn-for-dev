b55d8b4b74d723c1b0ba478f88aa0a8f36077124
VjacheslavZ <slavikov.net@mail.ru>
2018-08-10 00:34:37 +0300

    add component AddExperience

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dasboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 //check token
 if(localStorage.jwtToken) {
@@ -61,9 +62,12 @@ class App extends Component {
                             <Switch>
                                 <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
                             </Switch>
-	                        <Switch>
-		                        <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
-	                        </Switch>
+                            <Switch>
+                                <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute exact path='/add-experience' component={AddExperience}/>
+                            </Switch>
                         </div>
                         <Footer />
                     </div>
